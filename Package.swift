f7da25397eae8ed676a51a808a15d3b26f9688a9
Marcelo Esperidiao <m@rcelo.es>
2024-05-02 16:44:36 +1000

    Add Hummingbird dependency

--- Package.swift
+++ Package.swift
@@ -4,14 +4,20 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftRegistry",
+    platforms: [ .macOS(.v14) ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.1"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0-beta.4"),
     ],
     targets: [
         .executableTarget(
             name: "SwiftRegistry",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Hummingbird", package: "hummingbird"),
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
             ]
         ),
     ]
