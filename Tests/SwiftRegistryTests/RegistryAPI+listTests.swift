e31e1cc36725d7119b4b1056b6e0c90200dea810
Marcelo Esperidiao <m@rcelo.es>
2024-06-24 17:48:34 +1000

    Migrate tests to swift-test

--- Tests/SwiftRegistryTests/RegistryAPI+listTests.swift
+++ Tests/SwiftRegistryTests/RegistryAPI+listTests.swift
@@ -1,131 +1,81 @@
-@testable import SwiftRegistry
 import OpenAPIRuntime
 import RegistryAPI
-import XCTest
+@testable import SwiftRegistry
+import Testing
 
-final class RegistryAPI_listPackageReleasesTests: XCTestCase {
+@Test func listPackages() async throws {
+    let mockRepository = ReleaseMemoryRepository(releases: .mock())
+    let testSubject = SwiftRegistryAPI(repository: mockRepository)
 
-    typealias Input = Operations.listPackageReleases.Input
-    typealias Output = Operations.listPackageReleases.Output
+    let input = Operations.listPackageReleases.Input(
+        path: .init(scope: "mona", name: "LinkedList")
+    )
+    let output = try await testSubject.listPackageReleases(input)
 
-    var mockRepository: ReleaseMemoryRepository!
-    var testSubject: SwiftRegistryAPI<ReleaseMemoryRepository>!
+    let okOutput = try #require(try output.ok)
 
-    /// Test a call to the list package releases endpoint.
-    ///
-    /// The response should be compliant to the specification at 
-    /// https://github.com/apple/swift-package-manager/blob/main/Documentation/PackageRegistry/Registry.md#41-list-package-releases 
-    ///
-    /// ```
-    /// HTTP/1.1 200 OK
-    /// Content-Type: application/json
-    /// Content-Version: 1
-    /// Content-Length: 508
-    /// Link: <https://github.com/mona/LinkedList>; rel="canonical",
-    ///       <ssh://git@github.com:mona/LinkedList.git>; rel="alternate",
-    ///       <https://packages.example.com/mona/LinkedList/1.1.1>; rel="latest-version",
-    ///       <https://github.com/sponsors/mona>; rel="payment"
-    /// 
-    /// {
-    ///     "releases": {
-    ///         "1.1.1": {
-    ///             "url": "https://packages.example.com/mona/LinkedList/1.1.1"
-    ///         },
-    ///         "1.1.0": {
-    ///             "url": "https://packages.example.com/mona/LinkedList/1.1.0",
-    ///             "problem": {
-    ///                 "status": 410,
-    ///                 "title": "Gone",
-    ///                 "detail": "this release was removed from the registry"
-    ///             }
-    ///         },
-    ///         "1.0.0": {
-    ///             "url": "https://packages.example.com/mona/LinkedList/1.0.0"
-    ///         }
-    ///     }
-    /// }
-    /// ```
-    func testListPackageReleases() async throws {
-        mockRepository = ReleaseMemoryRepository(releases: .mock())
-        testSubject = .init(repository: ReleaseMemoryRepository())
+    // Content-Type: application/json
+    // TODO
 
-        let input = Input(
-            path: .init(scope: "mona", name: "LinkedList")
-        )
-        let output = try await testSubject.listPackageReleases(input)
+    // Content-Version: 1
+    #expect(okOutput.headers.Content_hyphen_Version == ._1)
 
-        guard case .ok(let ok) = output else {
-            XCTFail("The list package releases call must succeed")
-            return
-        }
+    // Content-Length: 508
+    #expect(okOutput.headers.Content_hyphen_Length == 508)
 
-        // Content-Type: application/json
-        // TODO
+    // Link: <https://github.com/mona/LinkedList>; rel="canonical",
+    //       <ssh://git@github.com:mona/LinkedList.git>; rel="alternate",
+    //       <https://packages.example.com/mona/LinkedList/1.1.1>; rel="latest-version",
+    //       <https://github.com/sponsors/mona>; rel="payment"
+    // TODO
 
-        // Content-Version: 1
-        XCTAssertEqual(ok.headers.Content_hyphen_Version, ._1)
+    // Body
+    let jsonBody = try #require(try okOutput.body.json)
 
-        // Content-Length: 508
-        XCTAssertEqual(ok.headers.Content_hyphen_Length, 508)
+    let releases = jsonBody.releases.value
+    #expect(releases.keys.count == 3)
 
-        // Link: <https://github.com/mona/LinkedList>; rel="canonical",
-        //       <ssh://git@github.com:mona/LinkedList.git>; rel="alternate",
-        //       <https://packages.example.com/mona/LinkedList/1.1.1>; rel="latest-version",
-        //       <https://github.com/sponsors/mona>; rel="payment"
-        // TODO
+    //  "1.1.1": {
+    //      "url": "https://packages.example.com/mona/LinkedList/1.1.1"
+    //  },
+    let release_1_1_1 = try #require(releases["1.1.1"] as? OpenAPIObjectContainer)
+    #expect(release_1_1_1.value.keys.count == 1)
 
-        // Body
-        guard case .json(let json) = ok.body else {
-            XCTFail("The list package releases call must return a JSON.")
-            return
-        }
+    let release_1_1_1URL = try #require(release_1_1_1.value["url"] as? String)
+    #expect(release_1_1_1URL == "https://packages.example.com/mona/LinkedList/1.1.1")
 
-        let releases = json.releases.value
-        XCTAssertEqual(releases.keys.count, 3)
+    // "1.1.0": {
+    //     "url": "https://packages.example.com/mona/LinkedList/1.1.0",
+    //     "problem": {
+    //         "status": 410,
+    //         "title": "Gone",
+    //         "detail": "this release was removed from the registry"
+    //     }
+    // },
+    let release_1_1_0 = try #require(releases["1.1.0"] as? OpenAPIObjectContainer)
+    #expect(release_1_1_0.value.keys.count == 2)
 
-        //  "1.1.1": {
-        //      "url": "https://packages.example.com/mona/LinkedList/1.1.1"
-        //  },
-        let release_1_1_1 = try XCTUnwrap(releases["1.1.1"] as? OpenAPIObjectContainer)
-        XCTAssertEqual(release_1_1_1.value.keys.count, 1)
+    let release_1_1_0URL = try #require(release_1_1_0.value["url"] as? String)
+    #expect(release_1_1_0URL == "https://packages.example.com/mona/LinkedList/1.1.0")
 
-        let release_1_1_1URL = try XCTUnwrap(release_1_1_1.value["url"] as? String)
-        XCTAssertEqual(release_1_1_1URL, "https://packages.example.com/mona/LinkedList/1.1.1")
+    let release_1_1_0Problem = try #require(release_1_1_0.value["problem"] as? OpenAPIObjectContainer)
+    #expect(release_1_1_0Problem.value.keys.count == 3)
 
-        // "1.1.0": {
-        //     "url": "https://packages.example.com/mona/LinkedList/1.1.0",
-        //     "problem": {
-        //         "status": 410,
-        //         "title": "Gone",
-        //         "detail": "this release was removed from the registry"
-        //     }
-        // },
-        let release_1_1_0 = try XCTUnwrap(releases["1.1.0"] as? OpenAPIObjectContainer)
-        XCTAssertEqual(release_1_1_0.value.keys.count, 2)
+    let release_1_1_0ProblemStatus = try #require(release_1_1_0Problem.value["status"] as? Int)
+    #expect(release_1_1_0ProblemStatus == 410)
 
-        let release_1_1_0URL = try XCTUnwrap(release_1_1_0.value["url"] as? String)
-        XCTAssertEqual(release_1_1_0URL, "https://packages.example.com/mona/LinkedList/1.1.0")
-        
-        let release_1_1_0Problem = try XCTUnwrap(release_1_1_0.value["problem"] as? OpenAPIObjectContainer)
-        XCTAssertEqual(release_1_1_0Problem.value.keys.count, 3)
-        
-        let release_1_1_0ProblemStatus = try XCTUnwrap(release_1_1_0Problem.value["status"] as? Int)
-        XCTAssertEqual(release_1_1_0ProblemStatus, 410)
+    let release_1_1_0ProblemTitle = try #require(release_1_1_0Problem.value["title"] as? String)
+    #expect(release_1_1_0ProblemTitle == "Gone")
 
-        let release_1_1_0ProblemTitle = try XCTUnwrap(release_1_1_0Problem.value["title"] as? String)
-        XCTAssertEqual(release_1_1_0ProblemTitle, "Gone")
+    let release_1_1_0ProblemDetail = try #require(release_1_1_0Problem.value["detail"] as? String)
+    #expect(release_1_1_0ProblemDetail == "this release was removed from the registry")
 
-        let release_1_1_0ProblemDetail = try XCTUnwrap(release_1_1_0Problem.value["detail"] as? String)
-        XCTAssertEqual(release_1_1_0ProblemDetail, "this release was removed from the registry")
+    // "1.0.0": {
+    //     "url": "https://packages.example.com/mona/LinkedList/1.0.0"
+    // }
+    let release_1_0_0 = try #require(releases["1.0.0"] as? OpenAPIObjectContainer)
+    #expect(release_1_0_0.value.keys.count == 1)
 
-        // "1.0.0": {
-        //     "url": "https://packages.example.com/mona/LinkedList/1.0.0"
-        // }
-        let release_1_0_0 = try XCTUnwrap(releases["1.0.0"] as? OpenAPIObjectContainer)
-        XCTAssertEqual(release_1_0_0.value.keys.count, 1)
-
-        let release_1_0_0URL = try XCTUnwrap(release_1_0_0.value["url"] as? String)
-        XCTAssertEqual(release_1_0_0URL, "https://packages.example.com/mona/LinkedList/1.0.0")
-    }
-
+    let release_1_0_0URL = try #require(release_1_0_0.value["url"] as? String)
+    #expect(release_1_0_0URL == "https://packages.example.com/mona/LinkedList/1.0.0")
 }
